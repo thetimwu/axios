7df00405cf3b7084f39c5a02c6c61f8b15e80e38
tim wu <timwuuk@gmail.com>
2019-11-15 06:50:54 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error); //so can handler error locally
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
